b8b0f44c97c4eacc44654b5960dea463e5331bc2
d31003 <pohanli@utexas.edu>
2023-03-08 22:31:08 -0600

    fix all yolov1 bugs

---
+++ dtac/object_detection/yolo_model.py
@@ -0,0 +1,193 @@
+from PIL import Image
+import torch
+import torch.nn as nn
+from dtac.object_detection.od_utils import *
+
+architecture_config = [
+    #Tuple: (kernel_size, number of filters, strides, padding)
+    (7, 64, 2, 3),
+    #"M" = Max Pool Layer
+    "M",
+    (3, 192, 1, 1),
+    "M",
+    (1, 128, 1, 0),
+    (3, 256, 1, 1),
+    (1, 256, 1, 0),
+    (3, 512, 1, 1),
+    "M",
+    #List: [(tuple), (tuple), how many times to repeat]
+    [(1, 256, 1, 0), (3, 512, 1, 1), 4],
+    (1, 512, 1, 0),
+    (3, 1024, 1, 1),
+    "M",
+    [(1, 512, 1, 0), (3, 1024, 1, 1), 2],
+    (3, 1024, 1, 1),
+    (3, 1024, 2, 1),
+    (3, 1024, 1, 1),
+    (3, 1024, 1, 1),
+    #Doesnt include fc layers
+]
+
+class CNNBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, **kwargs):
+        super(CNNBlock, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, bias=False, **kwargs)
+        self.batchnorm = nn.BatchNorm2d(out_channels)
+        self.leakyrelu = nn.LeakyReLU(0.1)
+        
+    def forward(self, x):
+        return self.leakyrelu(self.batchnorm(self.conv(x)))
+    
+class YoloV1(nn.Module):
+    def __init__(self, in_channels=3, **kwargs):
+        super(YoloV1, self).__init__()
+        self.architecture = architecture_config
+        self.in_channels = in_channels
+        self.darknet = self._create_conv_layers(self.architecture)
+        self.fcs = self._create_fcs(**kwargs)
+        
+    def forward(self, x):
+        x = self.darknet(x)
+        return self.fcs(torch.flatten(x, start_dim=1))
+    
+    def _create_conv_layers(self, architecture):
+        layers = []
+        in_channels = self.in_channels
+        
+        for x in architecture:
+            if type(x) == tuple:
+                layers += [CNNBlock(in_channels, x[1], kernel_size=x[0], stride=x[2], padding=x[3])]
+                in_channels = x[1]
+            elif type(x) == str:
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            elif type(x) == list:
+                conv1 = x[0] #Tuple
+                conv2 = x[1] #Tuple
+                repeats = x[2] #Int
+                
+                for _ in range(repeats):
+                    layers += [CNNBlock(in_channels, conv1[1], kernel_size=conv1[0], stride=conv1[2], padding=conv1[3])]
+                    layers += [CNNBlock(conv1[1], conv2[1], kernel_size=conv2[0], stride=conv2[2], padding=conv2[3])]
+                    in_channels = conv2[1]
+                    
+        return nn.Sequential(*layers)
+    
+    def _create_fcs(self, split_size, num_boxes, num_classes):
+        S, B, C = split_size, num_boxes, num_classes
+        return nn.Sequential(nn.Flatten(), nn.Linear(1024 * S * S, 496), nn.Dropout(0.0), nn.LeakyReLU(0.1), nn.Linear(496, S * S * (C + B * 5)))#Original paper uses nn.Linear(1024 * S * S, 4096) not 496. Also the last layer will be reshaped to (S, S, 13) where C+B*5 = 13
+    
+
+class YoloLoss(nn.Module):
+    """
+    Calculate the loss for yolo (v1) model
+    """
+
+    def __init__(self, S=7, B=2, C=3):
+        super(YoloLoss, self).__init__()
+        self.mse = nn.MSELoss(reduction="sum")
+
+        """
+        S is split size of image (in paper 7),
+        B is number of boxes (in paper 2),
+        C is number of classes (in paper 20, in dataset 3),
+        """
+        self.S = S
+        self.B = B
+        self.C = C
+
+        # These are from Yolo paper, signifying how much we should
+        # pay loss for no object (noobj) and the box coordinates (coord)
+        self.lambda_noobj = 0.5
+        self.lambda_coord = 5
+
+    def forward(self, predictions, target):
+        # predictions are shaped (BATCH_SIZE, S*S(C+B*5) when inputted
+        predictions = predictions.reshape(-1, self.S, self.S, self.C + self.B * 5)
+
+        # Calculate IoU for the two predicted bounding boxes with target bbox
+        iou_b1 = intersection_over_union(predictions[..., self.C + 1:self.C + 5], target[..., self.C + 1:self.C + 5])
+        iou_b2 = intersection_over_union(predictions[..., self.C + 6:self.C + 10], target[..., self.C + 1:self.C + 5])
+        ious = torch.cat([iou_b1.unsqueeze(0), iou_b2.unsqueeze(0)], dim=0)
+
+        # Take the box with highest IoU out of the two prediction
+        # Note that bestbox will be indices of 0, 1 for which bbox was best
+        iou_maxes, bestbox = torch.max(ious, dim=0)
+        exists_box = target[..., self.C].unsqueeze(3)  # in paper this is Iobj_i
+
+        # ======================== #
+        #   FOR BOX COORDINATES    #
+        # ======================== #
+
+        # Set boxes with no object in them to 0. We only take out one of the two 
+        # predictions, which is the one with highest Iou calculated previously.
+        box_predictions = exists_box * (
+            (
+                bestbox * predictions[..., self.C + 6:self.C + 10]
+                + (1 - bestbox) * predictions[..., self.C + 1:self.C + 5]
+            )
+        )
+
+        box_targets = exists_box * target[..., self.C + 1:self.C + 5]
+
+        # Take sqrt of width, height of boxes to ensure that
+        box_predictions[..., 2:4] = torch.sign(box_predictions[..., 2:4]) * torch.sqrt(
+            torch.abs(box_predictions[..., 2:4] + 1e-6)
+        )
+        box_targets[..., 2:4] = torch.sqrt(box_targets[..., 2:4])
+
+        box_loss = self.mse(
+            torch.flatten(box_predictions, end_dim=-2),
+            torch.flatten(box_targets, end_dim=-2),
+        )
+
+        # ==================== #
+        #   FOR OBJECT LOSS    #
+        # ==================== #
+
+        # pred_box is the confidence score for the bbox with highest IoU
+        pred_box = (
+            bestbox * predictions[..., self.C + 5:self.C + 6] + (1 - bestbox) * predictions[..., self.C:self.C + 1]
+        )
+
+        object_loss = self.mse(
+            torch.flatten(exists_box * pred_box),
+            torch.flatten(exists_box * target[..., self.C:self.C + 1]),
+        )
+
+        # ======================= #
+        #   FOR NO OBJECT LOSS    #
+        # ======================= #
+
+        #max_no_obj = torch.max(predictions[..., 20:21], predictions[..., 25:26])
+        #no_object_loss = self.mse(
+        #    torch.flatten((1 - exists_box) * max_no_obj, start_dim=1),
+        #    torch.flatten((1 - exists_box) * target[..., 20:21], start_dim=1),
+        #)
+
+        no_object_loss = self.mse(
+            torch.flatten((1 - exists_box) * predictions[..., self.C:self.C + 1], start_dim=1),
+            torch.flatten((1 - exists_box) * target[..., self.C:self.C + 1], start_dim=1),
+        )
+
+        no_object_loss += self.mse(
+            torch.flatten((1 - exists_box) * predictions[..., self.C + 5:self.C + 6], start_dim=1),
+            torch.flatten((1 - exists_box) * target[..., self.C:self.C + 1], start_dim=1)
+        )
+
+        # ================== #
+        #   FOR CLASS LOSS   #
+        # ================== #
+
+        class_loss = self.mse(
+            torch.flatten(exists_box * predictions[..., :self.C], end_dim=-2,),
+            torch.flatten(exists_box * target[..., :self.C], end_dim=-2,),
+        )
+
+        loss = (
+            self.lambda_coord * box_loss  # first two rows in paper
+            + object_loss  # third row in paper
+            + self.lambda_noobj * no_object_loss  # forth row
+            + class_loss  # fifth row
+        )
+
+        return loss
